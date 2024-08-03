36629f907e036fa9f54961f849cffbd943933130
vampire118 <shigerumiyamoto.prodev9413@gmail.com>
2024-08-03 10:09:42 -0700

    feat: add dashboard

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,16 +6,20 @@ import SecondComponent from './components/SecondComponent'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Test from './pages/Test'
 import SignIn from './pages/sign-in/SignIn'
+import LandingPage from './pages/landing-page/LandingPage'
 import SignUp from './pages/sign-up/SignUp'
+import { Dashboard } from '@mui/icons-material'
+
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
